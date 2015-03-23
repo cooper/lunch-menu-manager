ce4a09a245bb1ad6f26bac94a7a18a50dff1e6cf
Mitchell Cooper <githubspam@rlygd.net>
2015-03-23 12:16:26 -0400

    calendar page

---
+++ calendar.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Menu calendar</title>
+    <link href="styles/calendar.css" type="text/css" rel="stylesheet" />
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,600" rel="stylesheet" type="text/css" />
+    <script type="text/javascript" src="scripts/mootools.js"></script>
+    <script type="text/javascript" src="scripts/calendar.js"></script>
+</head>
+<body>
+    <?php
+        require_once('templates/calendar.php');
+    ?>
+</body>
+</html>

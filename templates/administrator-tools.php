82dfd65506a7b2fe1f316d090d2e0f414ba72659
Mitchell Cooper <githubspam@rlygd.net>
2015-03-23 12:43:52 -0400

    tooltips

--- templates/administrator-tools.php
+++ templates/administrator-tools.php
@@ -31,12 +31,12 @@ function nextMonth() {
 <div class="administrator-tools-container">
     <div style="width: 1000px; margin: auto;">
         <ul class="administrator-tools">
-            <li><a href="<?php echo previousMonth(); ?>">&larr;</a></li>
-            <li><a href="<?php echo nextMonth(); ?>">&rarr;</a></li>
-            <li><a id="mode-trigger" href="#">Breakfast</a></li>
-            <li><a id="print-button" href="#">Print</a></li>
-            <li><a id="email-button" href="#">E-mail</a></li>
-            <li><a href="#">Log out</a></li>
+            <li><a href="<?php echo previousMonth(); ?>" title="Previous month">&larr;</a></li>
+            <li><a href="<?php echo nextMonth(); ?>" title="Next month">&rarr;</a></li>
+            <li><a id="mode-trigger" href="#" title="Toggle between breakfast and lunch">Breakfast</a></li>
+            <li><a id="print-button" href="#" title="Print this month's menu">Print</a></li>
+            <li><a id="email-button" href="#" title="Send this month's menu via email">E-mail</a></li>
+            <li><a href="#" title="Log out of administrator panel">Log out</a></li>
         </ul>
     </div>
 </div>

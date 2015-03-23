44ca5178bca454258844c9ecb9e1c28ee7fd9118
Mitchell Cooper <githubspam@rlygd.net>
2015-03-23 16:59:44 -0400

    check for requirements

--- functions/update-menu.php
+++ functions/update-menu.php
@@ -4,6 +4,12 @@ $db = new SQLite3('../db/menu.db');
 if (!$db)
     die("Opening database failed");
 
+foreach ($required in array('year', 'month', 'day', 'breakfast', 'lunch', 'salad')) {
+    if (isset($_POST[$required]))
+        continue;
+    die("Missing required option '$required'");
+}
+
 $db->query('CREATE TABLE IF NOT EXISTS menu (year INT, month INT, day INT, breakfast TEXT, lunch TEXT, salad TEXT, set_timestamp INT)');
 
 // delete any previous records
@@ -21,6 +27,7 @@ $st->bindValue(':day',          intval($_POST['day']),   SQLITE3_INTEGER);
 $st->bindValue(':breakfast',    $_POST['breakfast'],     SQLITE3_TEXT   );
 $st->bindValue(':lunch',        $_POST['lunch'],         SQLITE3_TEXT   );
 $st->bindValue(':salad',        $_POST['salad'],         SQLITE3_TEXT   );
+$st->bindValue(':timestamp',    time(),                  SQLITE3_INTEGER);
 $st->execute();
 
 ?>

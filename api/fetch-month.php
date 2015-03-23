510b4ce68f03ec72b112d468ae65afb1d4c02637
Mitchell Cooper <githubspam@rlygd.net>
2015-03-23 11:36:20 -0400

    quote these

--- api/fetch-month.php
+++ api/fetch-month.php
@@ -7,9 +7,9 @@ $month     = isset($_GET['month']) ? $_GET['month'] + 0 : date('n');
 $month     = $month % 12;
 
 echo json_encode(array(
-    '3-2-2015' => array(breakfast => 'a1', lunch => 'a2'),
-    '3-3-2015' => array(breakfast => 'b1', lunch => 'b2'),
-    '3-4-2015' => array(breakfast => 'c1', lunch => 'c2')
+    '3-2-2015' => array('breakfast' => 'a1', 'lunch' => 'a2'),
+    '3-3-2015' => array('breakfast' => 'b1', 'lunch' => 'b2'),
+    '3-4-2015' => array('breakfast' => 'c1', 'lunch' => 'c2')
 ));
 
 ?>
--- templates/administrator-tools.php
+++ templates/administrator-tools.php
@@ -31,7 +31,7 @@ function nextMonth() {
 <div class="administrator-tools-container">
     <div style="width: 1000px; margin: auto;">
         <ul class="administrator-tools">
-            <li><a id="email-button" href="<? php echo previousMonth(); ?>">&larr;</a></li>
+            <li><a id="email-button" href="<?php echo previousMonth(); ?>">&larr;</a></li>
             <li><a id="email-button" href="<?php echo nextMonth(); ?>">&rarr;</a></li>
             <li><a id="mode-trigger" href="#">Breakfast</a></li>
             <li><a id="print-button" href="#">Print</a></li>

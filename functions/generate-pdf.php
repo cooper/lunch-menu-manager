668aa928a75f5d9fb0282282d5265eb2c27e4cc6
Mitchell Cooper <githubspam@rlygd.net>
2015-03-23 15:23:56 -0400

    lose the debug

--- functions/generate-pdf.php
+++ functions/generate-pdf.php
@@ -11,7 +11,6 @@ array_pop($calendar_php); // remove functions/
 $calendar_php = implode($calendar_php, '/') . '/calendar.php';
 
 $pdf = new Pdf("http://localhost:{$_SERVER['SERVER_PORT']}$calendar_php?year={$_GET['year']}&month={$_GET['month']}");
-print_r($pdf);
 $pdf->saveAs('../cache/menu.pdf');
 $pdf->send('menu.pdf');
 

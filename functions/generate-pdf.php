acc2c107b0bced1ff2f1536b10171a5979f8142e
Mitchell Cooper <githubspam@rlygd.net>
2015-03-23 15:22:14 -0400

    detemrine URI

--- functions/generate-pdf.php
+++ functions/generate-pdf.php
@@ -4,7 +4,15 @@ require_once('session.php');
 require_once('vendor/autoload.php');
 use mikehaertl\wkhtmlto\Pdf;
 
-$pdf = new Pdf('http://localhost:'.$_SERVER['SERVER_PORT']."/calendar.php?year={$_GET['year']}&month={$_GET['month']}");
+// determine URI to calendar.php
+$calendar_php = explode($_SERVER['DOCUMENT_URI'], '/');
+array_pop($calendar_php); // remove generate-pdf.php
+array_pop($calendar_php); // remove functions/
+$calendar_php = implode($calendar_php, '/') . '/calendar.php';
+
+    echo $calendar_php."\n";
+
+$pdf = new Pdf('http://localhost:'.$_SERVER['SERVER_PORT']."$calendar_php?year={$_GET['year']}&month={$_GET['month']}");
 print_r($_SERVER);
 print_r($pdf);
 $pdf->saveAs('../cache/menu.pdf');

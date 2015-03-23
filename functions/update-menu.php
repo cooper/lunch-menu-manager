736feb92e14d0462ef6c57f867785ce2734216ea
Mitchell Cooper <githubspam@rlygd.net>
2015-03-23 17:19:38 -0400

    get rid of 7

--- functions/update-menu.php
+++ functions/update-menu.php
@@ -20,7 +20,7 @@ $st->bindValue(':day',   intval($_POST['day']),      SQLITE3_INTEGER);
 $st->execute();
 
 // insert the new records
-$db->prepare('INSERT INTO menu (year, month, day, breakfast, lunch, salad, set_timestamp) VALUES (:year, :month, :day, :breakfast, :lunch, :salad, :timestamp)');7
+$db->prepare('INSERT INTO menu (year, month, day, breakfast, lunch, salad, set_timestamp) VALUES (:year, :month, :day, :breakfast, :lunch, :salad, :timestamp)');
 $st->bindValue(':year',         intval($_POST['year']),  SQLITE3_INTEGER);
 $st->bindValue(':month',        intval($_POST['month']), SQLITE3_INTEGER);
 $st->bindValue(':day',          intval($_POST['day']),   SQLITE3_INTEGER);

7cde7f9c1b47a31843dea2d84dff8f600dc8f064
Mitchell Cooper <githubspam@rlygd.net>
2015-03-23 11:34:28 -0400

    implement month switchers

--- templates/administrator-tools.php
+++ templates/administrator-tools.php
@@ -1,8 +1,38 @@
+<?php
+
+function previousMonth() {
+    global $year, $month;
+    $newYear  = $year;
+    $newMonth = $month;
+    if ($month == 1) {
+        $newYear--;
+        $newMonth = 12;
+    }
+    else
+        $newMonth--;
+    return __FILE__."?year=$newYear&month=$newMonth";
+}
+
+function nextMonth() {
+    global $year, $month;
+    $newYear  = $year;
+    $newMonth = $month;
+    if ($month == 12) {
+        $newYear++;
+        $newMonth = 1;
+    }
+    else
+        $newMonth++;
+    return __FILE__."?year=$newYear&month=$newMonth";
+}
+
+?>
+
 <div class="administrator-tools-container">
     <div style="width: 1000px; margin: auto;">
         <ul class="administrator-tools">
-            <li><a id="email-button" href="#">&larr;</a></li>
-            <li><a id="email-button" href="#">&rarr;</a></li>
+            <li><a id="email-button" href="<? php echo previousMonth(); ?>">&larr;</a></li>
+            <li><a id="email-button" href="<?php echo nextMonth(); ?>">&rarr;</a></li>
             <li><a id="mode-trigger" href="#">Breakfast</a></li>
             <li><a id="print-button" href="#">Print</a></li>
             <li><a id="email-button" href="#">E-mail</a></li>

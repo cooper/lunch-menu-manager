94360ea50b4d7a9139cec9191aaa69ee6fa335a0
Mitchell Cooper <githubspam@rlygd.net>
2015-03-23 11:43:40 -0400

    fix

--- templates/administrator-tools.php
+++ templates/administrator-tools.php
@@ -31,12 +31,12 @@ function nextMonth() {
 <div class="administrator-tools-container">
     <div style="width: 1000px; margin: auto;">
         <ul class="administrator-tools">
-            <li><a id="email-button" href="<?php echo previousMonth(); ?>">&larr;</a></li>
-            <li><a id="email-button" href="<?php echo nextMonth(); ?>">&rarr;</a></li>
+            <li><a href="<?php echo previousMonth(); ?>">&larr;</a></li>
+            <li><a href="<?php echo nextMonth(); ?>">&rarr;</a></li>
             <li><a id="mode-trigger" href="#">Breakfast</a></li>
             <li><a id="print-button" href="#">Print</a></li>
             <li><a id="email-button" href="#">E-mail</a></li>
-            <li><a id="email-button" href="#">Log out</a></li>
+            <li><a href="#">Log out</a></li>
         </ul>
     </div>
 </div>

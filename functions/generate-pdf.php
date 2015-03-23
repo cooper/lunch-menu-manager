667dc4d9e79e7bb5d16335fc7f3fbcde65a6aab4
Mitchell Cooper <githubspam@rlygd.net>
2015-03-23 15:29:57 -0400

    print errors

--- functions/generate-pdf.php
+++ functions/generate-pdf.php
@@ -11,8 +11,15 @@ array_pop($calendar_php); // remove functions/
 $calendar_php = implode($calendar_php, '/') . '/calendar.php';
 
 $pdf = new Pdf("http://localhost:{$_SERVER['SERVER_PORT']}$calendar_php?year={$_GET['year']}&month={$_GET['month']}");
+// $pdf->binary = 'C:\path\to\wkhtmltopdf';
 $pdf->setOptions(array('orientation' => 'landscape'));
-$pdf->saveAs('../cache/menu.pdf');
-$pdf->send();
+
+if (!$pdf->saveAs('../cache/menu.pdf')) {
+    echo "save error: ".$pdf->getError();
+}
+
+if (!$pdf->send()) {
+    echo "send error: ".$pdf->getError();
+}
 
 ?>

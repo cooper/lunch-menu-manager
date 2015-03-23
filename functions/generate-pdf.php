cd2f10e5a31afca560d7b4a9c8da50bee8959146
Mitchell Cooper <githubspam@rlygd.net>
2015-03-23 15:14:27 -0400

    save in cache

--- functions/generate-pdf.php
+++ functions/generate-pdf.php
@@ -5,7 +5,7 @@ require_once('vendor/autoload.php');
 use mikehaertl\wkhtmlto\Pdf;
 
 $pdf = new Pdf('http://localhost:'.$_SERVER['SERVER_PORT']."/calendar.php?year={$_GET['year']}&month={$_GET['month']}");
-$pdf->saveAs('menu.pdf');
+$pdf->saveAs('../cache/menu.pdf');
 
 echo json_encode(array(
     'file' => 'menu.pdf'

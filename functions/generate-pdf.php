9e11b9f9a5297c4883f0d1f7491e856111239130
Mitchell Cooper <githubspam@rlygd.net>
2015-03-23 15:44:15 -0400

    use html2pdf

--- functions/generate-pdf.php
+++ functions/generate-pdf.php
@@ -9,17 +9,8 @@ $calendar_php = explode($_SERVER['DOCUMENT_URI'], '/');
 array_pop($calendar_php); // remove generate-pdf.php
 array_pop($calendar_php); // remove functions/
 $calendar_php = implode($calendar_php, '/') . '/calendar.php';
+$calendar_php = "http://{$_SERVER['SERVER_HOST']}:{$_SERVER['SERVER_PORT']}$calendar_php?year={$_GET['year']}&month={$_GET['month']}";
 
-$pdf = new Pdf("http://localhost:{$_SERVER['SERVER_PORT']}$calendar_php?year={$_GET['year']}&month={$_GET['month']}");
-// $pdf->binary = 'C:\path\to\wkhtmltopdf';
-$pdf->setOptions(array('orientation' => 'landscape'));
-
-if (!$pdf->saveAs('../cache/menu.pdf')) {
-    echo "save error: ".$pdf->getError();
-}
-
-if (!$pdf->send()) {
-    echo "send error: ".$pdf->getError();
-}
+header("Location: http://www.html2pdf.it/?url=$calendar_php&format=Letter&margin=0cm&orientation=landscape&filename=menu-{$_GET['month']}-{$_GET['year']}.pdf");
 
 ?>

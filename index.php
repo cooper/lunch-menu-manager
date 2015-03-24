352510ee962641556cb03002ab75ce791bb31fca
Mitchell Cooper <githubspam@rlygd.net>
2015-03-23 21:47:47 -0400

    password

--- index.php
+++ index.php
@@ -52,7 +52,7 @@
             </tr>
             <tr>
                 <td>Password</td>
-                <td><input type="text" name="password" /></td>
+                <td><input type="password" name="password" /></td>
             </tr>
             <tr>
                 <td><input type="submit" name="submit" value="Login" /></td>

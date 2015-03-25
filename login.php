b2059153379afa94f720e0a5b7d31b795820ad0e
Mitchell Cooper <githubspam@rlygd.net>
2015-03-24 23:29:33 -0400

    switch to red and black

--- login.php
+++ login.php
@@ -5,7 +5,7 @@ include('verify_login.php');
 
 if (verify_login($_POST['username'], $_POST['password'])) {
     $_SESSION['logged_in'] = true;
-    $_SESSION['theme'] = 'blue';
+    $_SESSION['theme'] = 'red-and-black';
     header('Location: administrator.php');
 }
 else header('Location: index.php');

17c01cb378dca88a41f46b51e5c6cbe78d9cedd9
Mitchell Cooper <githubspam@rlygd.net>
2015-03-30 14:28:27 -0400

    fetch day

---
+++ api/fetch-day.php
@@ -0,0 +1,29 @@
+<?php
+
+// fetch all the menu information for a given month
+
+require_once(__DIR__.'/../functions/date-input.php');
+
+$db = new SQLite3('../db/menu.db');
+if (!$db)
+    die("Opening database failed");
+
+$st = $db->prepare('SELECT * FROM menu WHERE year=? AND month=? AND day=?');
+$st->bindValue(1, $year,  SQLITE3_INTEGER);
+$st->bindValue(2, $month, SQLITE3_INTEGER);
+$st->bindValue(3, $day,   SQLITE3_INTEGER);
+$results = $st->execute();
+
+// turn into a JSON map
+$map = array(
+    'success'   => true,
+    'breakfast' => $row['breakfast'],
+    'lunch'     => $row['lunch'],
+    'salad'     => $row['salad'],
+    'timestamp' => $row['set_timestamp']
+);
+
+$json_result = json_encode($map);
+require_once('api-json.php');
+
+?>
--- functions/date-input.php
+++ functions/date-input.php
@@ -2,10 +2,11 @@
 
 $year      = isset($_GET['year'])  ? intval($_GET['year'])  : date('Y');
 $month     = isset($_GET['month']) ? intval($_GET['month']) : date('n');
+$day       = isset($_GET['day'])   ? intval($_GET['day'])   : date('j');
 $month     = $month % 12; if (!$month) $month = 12;
 $monthName = date('F', mktime(0, 0, 0, $month, 10));
 
-if ($month <= 0 || $year <= 0)
+if ($month <= 0 || $year <= 0 || $day <= 0 || $day > 31)
     die('Invalid date selection');
 
 ?>

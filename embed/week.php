1187cb220d701bf9d97c4fadb59803a0d837b60c
Mitchell Cooper <githubspam@rlygd.net>
2015-03-31 15:05:53 -0400

    new

--- embed/week.php
+++ embed/week.php
@@ -6,7 +6,7 @@ $monday = new DateTime('@'.strtotime('monday this week'));
 $friday = new DateTime('@'.strtotime('friday this week'));
 
 $interval = DateInterval::createFromDateString('day');
-$period = DatePeriod($monday, $interval, $friday);
+$period = new DatePeriod($monday, $interval, $friday);
 foreach ($period as $date) {
     echo $dt->format('Y-m-d');
 }

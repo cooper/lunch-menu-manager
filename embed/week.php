3480c05e23984d7d8929653d975d5cda99f66461
Mitchell Cooper <githubspam@rlygd.net>
2015-03-31 15:03:22 -0400

    use range

--- embed/week.php
+++ embed/week.php
@@ -2,11 +2,13 @@
 
 $year   = date('Y');
 $month  = date('n');
-$monday = date('j', strtotime('monday this week'));
-$friday = date('j', strtotime('friday this week'));
+$monday = new Date(strtotime('monday this week'));
+$friday = new Date(strtotime('friday this week'));
 
-foreach (range($monday, $friday) as $day) {
-    echo "$year-$month-$day<br>";
+$interval = DateInterval::createFromDateString('day');
+$period = DatePeriod($monday, $interval, $friday);
+foreach ($period as $date) {
+    echo $dt->format('Y-m-d');
 }
 
 ?>

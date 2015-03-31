e1b407d334ab4c1940e2e7be0e41fe78e1f67d82
Mitchell Cooper <githubspam@rlygd.net>
2015-03-31 15:04:01 -0400

    datetime

--- embed/week.php
+++ embed/week.php
@@ -2,8 +2,8 @@
 
 $year   = date('Y');
 $month  = date('n');
-$monday = new Date(strtotime('monday this week'));
-$friday = new Date(strtotime('friday this week'));
+$monday = new DateTime(strtotime('monday this week'));
+$friday = new DateTime(strtotime('friday this week'));
 
 $interval = DateInterval::createFromDateString('day');
 $period = DatePeriod($monday, $interval, $friday);

a7cba50ce6cc0c19d2e33e7459ec94f4c145f953
Mitchell Cooper <githubspam@rlygd.net>
2015-03-31 19:51:32 -0400

    embed week

--- embed/week.php
+++ embed/week.php
@@ -22,9 +22,60 @@ $friday = new DateTime('@'.strtotime('saturday this week'));
 $interval = DateInterval::createFromDateString('1 day');
 $period = new DatePeriod($monday, $interval, $friday);
 
-print_r($period);
-foreach ($period as $date) {
-    $day_data = $month_data[ $date->format('n-j-Y') ];
-}
-
 ?>
+
+<!doctype html>
+<html>
+
+<head>
+    <link href="http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet" type="text/css" />
+    <link href="../styles/red-and-black/weekly.css" rel="stylesheet" type="text/css" />
+</head>
+    
+<body>
+        <table id="weekly-calendar">
+<?php
+
+    foreach ($period as $date) {
+        $day_data = $month_data[ $date->format('n-j-Y') ];
+        if (!$day_data) $day_data = array();
+        
+        // lunch + salad
+        $lunch = $day_data['lunch'];
+        if (isset($lunch)) {
+            $salad = $day_data['salad'];
+            if (isset($salad))
+                $lunch .= "\n".$salad;
+            $lunch = str_replace("\n", "<br />", htmlentities($lunch));
+        }
+        else $lunch = 'N/A';
+        
+        // breakfast
+        $bfast = $day_data['breakfast'];
+        if (isset($bfast))
+            $bfast = str_replace("\n", "<br />", htmlentities($bfast));
+        else $bfast = 'N/A';
+        
+?>
+
+            <tr class="title">
+                <td colspan="2">
+                    <?php echo $date->format('l, F j'); ?>
+                </td>
+            </tr>
+            <tr class="day">
+                <td>
+                    <h3>Breakfast</h3>
+                    <?php echo $bfast_html; ?>
+                </td>
+                <td>
+                    <h3>Lunch</h3>
+                    <?php echo $lunch_html; ?>
+                </td>
+            </tr>
+            
+<?php } ?>
+        </table>
+</body>
+    
+</html>

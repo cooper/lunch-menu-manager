75e6123f266a9f2f271f197f44728cb3086527b7
Mitchell Cooper <githubspam@rlygd.net>
2015-03-31 15:11:30 -0400

    one day

--- embed/week.php
+++ embed/week.php
@@ -5,7 +5,7 @@ $month  = date('n');
 $monday = new DateTime('@'.strtotime('monday this week'));
 $friday = new DateTime('@'.strtotime('friday this week'));
 
-$interval = DateInterval::createFromDateString('day');
+$interval = DateInterval::createFromDateString('1 day');
 $period = new DatePeriod($monday, $interval, $friday);
 print_r($period);
 #foreach ($period as $date) {

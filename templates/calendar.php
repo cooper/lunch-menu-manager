fb8d87fcd711b8f132d46b450a59c6ddcba8836f
Mitchell Cooper <githubspam@rlygd.net>
2015-05-21 12:04:53 -0400

    today color

--- templates/calendar.php
+++ templates/calendar.php
@@ -35,7 +35,9 @@ function draw_calendar ($month, $year) {
         
         // ignore Saturday and Sunday
         if ($running_day != 0 && $running_day != 6) {
-            $calendar.= '<td data-year="'.$year.'" data-month="'.$month.'" data-day="'.$list_day.'" class="edit-button">';
+            $is_today = $date->format("$year-$month-$list_day") == date('Y-m-d');
+            $today_html = $is_today ? : ' today';
+            $calendar.= '<td data-year="'.$year.'" data-month="'.$month.'" data-day="'.$list_day.'" class="edit-button'.$today_html.'">';
             $calendar .= '<span class="day-number">'.$list_day.'</span>';
             $calendar .= '<span class="menu-items"></span>';
             $calendar .= '</td>';

d441f268cadcb5df0154c0edec1dff11fba0f9ff
Mitchell Cooper <githubspam@rlygd.net>
2016-03-15 22:56:29 -0400

    ugh

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -237,7 +237,7 @@ function refreshCalendar() {
         var menuDay   = td.retrieve('menuDay'),
             cellNotes = td.retrieve('cellNotes');
         if (!menuDay) {
-            if (typeof cellNotes != 'undefined' && cellNotes.length)
+            if (cellNotes != null && cellNotes.length)
                 td.getElement('.notes-items').setProperty('html',
                     replaceNewlines(cellNotes));
             return;

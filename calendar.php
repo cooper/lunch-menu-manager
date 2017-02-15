25d7e21abfdc4aa31199df79dc4f6a18d9af331b
Mitchell Cooper <githubspam@rlygd.net>
2017-02-15 16:40:15 -0500

    element data too

--- calendar.php
+++ calendar.php
@@ -15,7 +15,6 @@ $pdf = isset($_GET['pdf']) ? true : null;
     <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,600" rel="stylesheet" type="text/css" />
     <link rel="icon" type="image/png" href="images/favicon.png" />
     <script type="text/javascript" src="scripts/mootools.min.js"></script>
-    <script type="text/javascript" src="scripts/mootools-more.min.js"></script>
     <script type="text/javascript" src="scripts/calendar.js"></script>
 </head>
 <body>

e456f21c61bb9b36f294639af6eeb3e3d3461bfa
Mitchell Cooper <githubspam@rlygd.net>
2017-07-01 16:33:47 -0400

    forget .min

--- calendar.php
+++ calendar.php
@@ -14,7 +14,7 @@ $pdf = isset($_GET['pdf']) ? true : null;
     <link href="styles/<?= $theme ?>/calendar.css" type="text/css" rel="stylesheet" />
     <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,600" rel="stylesheet" type="text/css" />
     <link rel="icon" type="image/png" href="images/favicon.png" />
-    <script type="text/javascript" src="scripts/mootools.min.js"></script>
+    <script type="text/javascript" src="scripts/mootools.js"></script>
     <script type="text/javascript" src="scripts/calendar.js"></script>
 </head>
 <body>
